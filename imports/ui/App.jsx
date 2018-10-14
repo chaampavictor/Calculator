75410986d4137ea00a37789e9f87a1b375552b4d
Olivier JM <manolivier93@gmail.com>
2018-10-14 09:04:58 +0200

    added functions to all symbols

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Hello from './Hello.jsx';
 import SymbolButtons from './Symbols';
 
-const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <SymbolButtons />
-  </div>
-);
+class App extends React.Component{
+  state = {
+    counter: 0,
+    answer:0
+  }
+  increment = () => {
+    console.log('increment');
+  }
+  divide = () => {
+    console.log('divide');
+  }
+  decrement = () => {
+    console.log('decrement');
+  }
+  multiply = () => {
+    console.log('multiply');
+  }
+
+  render(){
+    return(
+      <Fragment>
+      <h1>Simple Calculator</h1>
+      <Hello />
+      <SymbolButtons 
+        add={() => this.increment()}
+        divide={() => this.divide()}
+        substract={() => this.decrement()}
+        multiply={() => this.multiply()}
+      />
+    </Fragment>
+    )
+  }
+}
 
 export default App;
